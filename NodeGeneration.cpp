9c453029424b0f4770b093dcf2a1dc031da29e97
Oli Legg <oliglegg@hotmail.co.uk>
2020-12-08 21:45:56 +0000

    Fixed bugs with graph redrawing lines

--- NodeGeneration.cpp
+++ NodeGeneration.cpp
@@ -8,18 +8,42 @@
 static std::vector<int> node_cache;
 
 // Returns an array of unique node ids that which your node id could be connected to
-void getConnectedNodes(std::vector<int>& values, const int maxConnections, int excludedNode)
+void getConnectedNodes(std::vector<Node>& nodes, Node& node)
 {
+    // Setting constants and helpers
+    const auto excludedNode = node.id;
+    const long int nodesLength = node_cache.size();
+    auto& alreadyConnected = node.connectedNodes;
+    const auto maxConnections = alreadyConnected.capacity() - alreadyConnected.size();
 
-    const auto nodesLength = node_cache.size();
-    values = node_cache;
-    if (excludedNode != nodesLength-1)
-        values[excludedNode] = nodesLength-1;
+    // Getting all the connected node ids and putting them into an array 
+    std::vector<int> valuesAlreadyConnected(alreadyConnected.size());
+    for (auto& node : alreadyConnected) valuesAlreadyConnected.emplace_back(node->id);
+    std::sort(valuesAlreadyConnected.begin(), valuesAlreadyConnected.end());
 
+    std::vector<int> values;
+
+    // Getting a list of all ids except for this node's id and connected ids
+    std::set_difference(
+        node_cache.begin(),
+        node_cache.end(),
+        valuesAlreadyConnected.begin(),
+        valuesAlreadyConnected.end(),
+        std::inserter(values, values.begin()));
+    const auto itr = std::find(values.begin(), values.end(), excludedNode);
+    if (itr != values.end())
+        values.erase(values.begin() + (itr - values.begin()));
+     
+    // Randomizing the array and taking the amount that we need.
     std::random_shuffle(values.begin(), values.end());
-
     values.resize(maxConnections);
     values.shrink_to_fit();
+
+    // Connect the random nodes to this node and this node to the random nodes
+    for (auto i = 0; i < maxConnections; i++) {
+        alreadyConnected.emplace_back(&nodes[values[i]]);
+        nodes[values[i]].connectedNodes.emplace_back(&node);
+    }
 }
 
 std::vector<Node> getNodes(int nNodes)
@@ -48,9 +72,8 @@ std::vector<Node> getNodes(int nNodes)
 
 void getNodesRandom(std::vector<Node> &nodes, const int width, const int height, unsigned int seed = NULL)
 {
-
-    const int nNodes = nodes.capacity();
-    node_cache.resize(nNodes);
+    long long int nNodes = nodes.capacity();
+    node_cache.resize(nNodes - 1);
     std::iota(node_cache.begin(), node_cache.end(), 0); // elements are set to [0...n-1]
     
     // Setting the seed
@@ -66,24 +89,12 @@ void getNodesRandom(std::vector<Node> &nodes, const int width, const int height,
         nodes[i].connectedNodes.reserve(connectedNodesN);
     }
 
-
-
-    // Creating the node connections
-    for (auto& node : nodes) {
-        // Min number of connections a node can have is 1.
-        // Max number of connections a node can have is n-1 because...
-        // a node cannot be connected to itself and...
-        // cannot be connected to the same node many times
-        // Nodes are bidirectional (A -> B, B -> A)
-
-        const auto connectedNodesN = node.connectedNodes.capacity();
-        const auto connectedNodesCurrent = node.connectedNodes.size();
-        std::vector<int> connections;
-        getConnectedNodes(connections, connectedNodesN, node.id);
-
-        for (auto i = 0; i < (connectedNodesN - connectedNodesCurrent); i++) {
-            node.connectedNodes.emplace_back(&nodes[connections[i]]);
-            nodes[connections[i]].connectedNodes.emplace_back(&node);
-        }
-    }
+    // - Creating the node connections
+    // Min number of connections a node can have is 1.
+    // Max number of connections a node can have is n-1 because...
+    // a node cannot be connected to itself and...
+    // cannot be connected to the same node many times
+    // Nodes are bidirectional (A -> B, B -> A)
+    // Graph must be complete
+    for (auto& node : nodes) getConnectedNodes(nodes, node);
 }
