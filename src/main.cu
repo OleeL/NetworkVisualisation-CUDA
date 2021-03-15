8db56a6358bdf32a98d9ddb0f28482f25f212ae6
Oli Legg <oliglegg@hotmail.co.uk>
2021-03-15 01:22:23 +0000

    cuda imported into proj

--- src/main.cu
+++ src/main.cu
@@ -1,9 +1,19 @@
+﻿#include <cuda_runtime.h>
+#include "device_launch_parameters.h"
 #include "Draw.hpp"
 #include "Startup.hpp"
 #include "NodeGeneration.hpp"
 #include <iostream>
 #include <cstdlib>
 #include "forceDirectedPlacement.hpp"
+
+#define gpuErrchk(ans) gpuAssert((ans), __FILE__, __LINE__);
+inline void gpuAssert(cudaError_t code, const char* file, int line, bool abort = true)
+{
+	if (code == cudaSuccess) return;
+	fprintf(stderr, "Error: %s %s Ln: %d\n", cudaGetErrorString(code), file, line);
+	if (abort) exit(code);
+}
 
 int main(int argc, char* argv[])
 {
